555260a1cdb83c275e5d1a694fb3521289c415ed
Tanya Agarwal <127618645+tanya21ag@users.noreply.github.com>
2023-11-03 15:10:40 +0530

    Delete pileofbooks directory

--- pileofbooks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PileofbooksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pileofbooks'
--- pileofbooks/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Pileofbooks(models.Model):
-  title = models.CharField(max_length=255)
-  author = models.CharField(max_length=255)
