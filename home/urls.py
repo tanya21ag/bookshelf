f817c25a89cee5798e32efb031ca6b8fc9b9d319
Tanya Agarwal <tanyagarwal210@gmail.com>
2023-11-09 16:13:16 +0530

    added files

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.home, name='home'),
     path("about/", views.about, name='about'),
-    path("faq/", views.faq, name='faq'),
     path("contact", views.contact, name='contact'),
     path("register/", views.register, name='register'),
     path("login/", views.user_login, name='login'),
